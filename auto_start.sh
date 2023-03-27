3ff9bd2ace677eca883e0f4daa33aff91ccb613d
brimon <1610937885@qq.com>
2023-03-27 19:37:28 +0800

    init

---
+++ auto_start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo chmod 777 /dev/ttyACM0
+
+
+# base
+source ~/masvision_v2/devel/setup.bash 
+{
+gnome-terminal -t "start1" -- bash -c "roslaunch roborts_bringup base.launch"
+}
+sleep 2s
+
+# us_cam
+source ~/masvision_v2/devel/setup.bash 
+{
+gnome-terminal -t "start2" -- bash -c "rosrun roborts_camera roborts_camera_node"
+}
+sleep 2s
+
+# armor_detect
+source ~/masvision_v2/devel/setup.bash 
+{
+gnome-terminal -t "start3" -- bash -c "roslaunch roborts_bringup armor_detection.launch"
+}
+sleep 2s
